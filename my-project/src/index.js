87c733f2373085df926b6b5f9bff4630987784c2
Arbaz Khan <arbazkhaann256@gmail.com>
2023-12-30 19:51:16 +0500

    2nd commit
    Removing double files

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from "./Pages/Home";
 import reportWebVitals from './reportWebVitals';
+import Home from "./Pages/Home";
 import {
   BrowserRouter,
   Routes,

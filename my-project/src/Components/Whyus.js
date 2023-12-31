db1949cfce63a0b16108995eedc807c13b34afcf
Arbaz Khan <arbazkhaann256@gmail.com>
2023-12-31 15:22:13 +0500

    New component

--- my-project/src/Components/Whyus.js
+++ my-project/src/Components/Whyus.js
@@ -6,25 +6,25 @@ const Whyus = () => {
     return (
         <>
 
-            <div className='my-32'>
-                <h1 className='text-3xl font-medium mx-24 text-center'>WHY CHOOSE US?</h1>
+            <div className='my-32 bg-[#F58E22] py-10'>
+                <h1 className='text-4xl font-bold mx-24 text-center '>WHY CHOOSE US?</h1>
 
 
-                <div className='flex flex-row items-center justify-center mx-14 my-8  gap-24 '>
+                <div className='flex flex-col items-center justify-center mx-14 my-8  gap-24 sm:flex-row '>
                     <div>
-                        <CardBlack heading={"Industry Transformation"} paragraph={"We aspire to redefine the landscape ofglobal logistics through continuous innovation and a dedication to excellence."} />
+                        <Card heading={"Industry Transformation"} paragraph={"We aspire to redefine the landscape ofglobal logistics through continuous innovation and a dedication to excellence."} />
                     </div>
                     <div>
                         <Card heading={"Global Reach"} paragraph={"With a focus on international logistics, we facilitate smooth operations for businesses worldwide."} />
                     </div>
 
                 </div>
-                <div className='grid grid-flow-row grid-cols-3 mx-14 my-8 gap-3 '>
+                <div className='flex flex-col items-center justify-center mx-14 my-8  gap-24 sm:flex-row  '>
                     <div>
                         <Card heading={"Innovative Solutions"} paragraph={"We lead the industry with cutting-edge global logistics solutions, ensuring seamless supply chain operations"} />
                     </div>
                     <div>
-                        <CardBlack heading={"Client-Centric Approach"} paragraph={"Our mission is centered aroundenabling client business growth, fostering success on a global scale."} />
+                        <Card heading={"Client-Centric Approach"} paragraph={"Our mission is centered aroundenabling client business growth, fostering success on a global scale."} />
 
                     </div>
                     <div>
